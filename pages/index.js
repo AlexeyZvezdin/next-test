bde8d7b78325f11ffa66806b96ee52e464dec76a
Alexey Zvezdin <alexeyrush94@gmail.com>
2020-04-08 14:15:28 +0400

    routes fix

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ const Index = () => (
   <header>
     <title>To my homepage</title>
     <h1>welcome</h1>
-    <a href="/about" style={{ display: 'block' }}>
+    <a href="./about" style={{ display: 'block' }}>
       Go to About
     </a>
   </header>
