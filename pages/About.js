50a7c46c424929645b27ffd8e8636ea76114acd1
Alexey Zvezdin <alexeyrush94@gmail.com>
2020-04-08 13:15:22 +0400

    Commit to deploy test

---
+++ pages/About.js
@@ -0,0 +1,15 @@
+export default () => {
+  return (
+    <>
+      <h1>About</h1>
+      <p>A js programmer</p>
+      <a style={{ display: 'block' }} href="/">
+        Go to home
+      </a>
+      <img
+        src="../static/img/240px-JavaScript-logo.png"
+        alt="javascript logo"
+      />
+    </>
+  );
+};
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,11 @@
-const Index = () => <h1>welcome</h1>;
+const Index = () => (
+  <header>
+    <title>To my homepage</title>
+    <h1>welcome</h1>
+    <a href="/about" style={{ display: 'block' }}>
+      Go to About
+    </a>
+  </header>
+);
 
 export default Index;
